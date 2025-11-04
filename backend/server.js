aed54f8363d3da35ea9759b190dbc2e214d30233
prem kumar <151729765+prem1kr@users.noreply.github.com>
2025-11-04 23:02:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: 'http://localhost:8081',
+  origin: 'https://full-stack-chat-app-react-native.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
